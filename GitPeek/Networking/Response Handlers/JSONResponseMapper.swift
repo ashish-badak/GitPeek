c8d3297337429f6d864ec7197f1336a6d1eb331f
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-15 02:18:10 +0530

    created default JSON decoder and using it to easy JSONResponseMapper initialisation

--- GitPeek/Networking/Response Handlers/JSONResponseMapper.swift
+++ GitPeek/Networking/Response Handlers/JSONResponseMapper.swift
@@ -20,3 +20,9 @@ struct JSONResponseMapper<Output: Decodable>: ResponseMapper {
         }
     }
 }
+
+extension JSONResponseMapper {
+    init() {
+        self.init(decoder: .default)
+    }
+}
---
+++ GitPeek/Utils/Extensions/JSONDecoderExtension.swift
@@ -0,0 +1,16 @@
+//
+//  JSONDecoderExtension.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 15/02/23.
+//
+
+import Foundation
+
+extension JSONDecoder {
+    static var `default`: JSONDecoder {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        return jsonDecoder
+    }
+}
