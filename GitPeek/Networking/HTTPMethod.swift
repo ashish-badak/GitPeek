4290ee390f7a3b9da4d4f0849d6e6ead16f2c0d7
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-15 01:34:01 +0530

    created HTTPMethod enum

---
+++ GitPeek/Networking/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 15/02/23.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
