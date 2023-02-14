7ee914a2410f18d0f0262f6749ddc55e9e254be9
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-15 01:40:19 +0530

    created APIRequestData

---
+++ GitPeek/Networking/APIRequestData.swift
@@ -0,0 +1,24 @@
+//
+//  APIRequestData.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 15/02/23.
+//
+
+import Foundation
+
+typealias Parameters = [String: Any]
+typealias HTTPHeaders = [String: String]
+
+protocol APIRequestData {
+    var baseURL: URL { get }
+    var path: String { get }
+    var httpMethod: HTTPMethod { get }
+    
+    var parameters: Parameters { get }
+    var encoder: ParameterEncoder { get }
+}
+
+extension APIRequestData {
+    var baseURL: URL { URL.GP.baseURL }
+}
