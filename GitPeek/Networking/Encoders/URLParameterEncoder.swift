ff261b89208df375f805f4b278b12dbc6fc7b531
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-15 01:46:42 +0530

    created URLParameterEncoder

---
+++ GitPeek/Networking/Encoders/URLParameterEncoder.swift
@@ -0,0 +1,32 @@
+//
+//  URLParameterEncoder.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 15/02/23.
+//
+
+import Foundation
+
+struct URLParameterEncoder: ParameterEncoder {
+    func encode(
+        urlRequest: inout URLRequest,
+        with parameters: Parameters
+    ) throws {
+        if parameters.isEmpty {
+            return
+        }
+        
+        guard let url = urlRequest.url,
+              var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        else {
+            // - TODO: Throw invalid URL error
+            return
+        }
+
+        urlComponents.queryItems = parameters.map {
+            URLQueryItem(name: $0.key, value: "\($0.value)")
+        }
+        urlRequest.url = urlComponents.url
+    }
+}
+
