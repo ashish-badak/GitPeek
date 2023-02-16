8b344300d71e15ca829681175bc85028d2338089
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-17 00:48:48 +0530

    allowing page size to be set for pagination with default value of 10

--- GitPeek/Networking/API Request Data/PaginationQueryParams.swift
+++ GitPeek/Networking/API Request Data/PaginationQueryParams.swift
@@ -8,9 +8,17 @@
 import Foundation
 
 class PaginationQueryParams: Encodable {
-    var page: Int
+    let page: Int
+    let pageSize: Int
+    static let defaultPageSize: Int = 10
     
-    init(page: Int) {
+    init(page: Int, pageSize: Int = PaginationQueryParams.defaultPageSize) {
         self.page = page
+        self.pageSize = pageSize
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case pageSize = "per_page"
     }
 }
