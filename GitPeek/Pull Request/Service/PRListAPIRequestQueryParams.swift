ce789664777ccab1f87c7f715b367a7ec752596b
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-17 01:09:34 +0530

    renamed PRListRequestQueryParams to PRListAPIRequestQueryParams

--- GitPeek/Pull Request/Service/PRListAPIRequestQueryParams.swift
+++ GitPeek/Pull Request/Service/PRListAPIRequestQueryParams.swift
@@ -1,5 +1,5 @@
 //
-//  PRListRequestQueryParams.swift
+//  PRListAPIRequestQueryParams.swift
 //  GitPeek
 //
 //  Created by Ashish Badak on 17/02/23.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-final class PRListRequestQueryParams: PaginationQueryParams {
+final class PRListAPIRequestQueryParams: PaginationQueryParams {
     let state: PullRequest.State?
     
     init(page: Int, state: PullRequest.State? = nil) {
