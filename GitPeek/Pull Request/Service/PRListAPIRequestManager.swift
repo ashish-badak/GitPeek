780c7a3e4fd4214e8529ab753114196e25d9247b
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-17 01:41:34 +0530

    created PRListAPIRequestManager to make api call for fetching pr list

---
+++ GitPeek/Pull Request/Service/PRListAPIRequestManager.swift
@@ -0,0 +1,33 @@
+//
+//  PRListAPIRequestManager.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 17/02/23.
+//
+
+import Foundation
+
+struct PRListAPIRequestManager {
+    private let restClient: RestClient
+    
+    init(restClient: RestClient = RestClient()) {
+        self.restClient = restClient
+    }
+    
+    func fetchPRListFrom(
+        repo: String,
+        ownedBy owner: String,
+        page: Int,
+        state: PullRequest.State,
+        then perform: @escaping (Result<[PullRequest], Error>) -> Void
+    ) {
+        let requestData = PRListAPIRequestData(
+            repo: repo,
+            owner: owner,
+            page: page,
+            state: state
+        )
+        
+        restClient.get(requestData: requestData, then: perform)
+    }
+}
