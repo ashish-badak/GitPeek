e960304db1a1d48414c4f9583ae41000eb443043
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-17 19:33:13 +0530

    added interactor and view as a dependancy inside presenter

--- GitPeek/Pull Request/Presenter/PullRequestListPresenter.swift
+++ GitPeek/Pull Request/Presenter/PullRequestListPresenter.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 final class PullRequestListPresenter: PullRequestListPresenterProtocol {
-    // - TODO: Add dependancies: view, interactor, router
+    private let interactor: PullRequestListInteractorProtocol
+    weak var view: PullRequestListViewProtocol?
+    
+    init(interactor: PullRequestListInteractorProtocol) {
+        self.interactor = interactor
+    }
     
     // - TODO: Build view models with presentation logic
     
