c26d6bd6b9c5639bc8df7fd96c8de11ee73a8c84
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-18 15:18:08 +0530

    created UserViewModel

---
+++ GitPeek/Pull Request/Presenter/View Models/UserViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  UserViewModel.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 18/02/23.
+//
+
+import Foundation
+
+final class UserViewModel {
+    let username: String
+    private let originalAvatarURL: URL?
+    var avatarURL: URL?
+    
+    init(user: User) {
+        self.username = user.username
+        self.originalAvatarURL = user.avatarUrl
+    }
+    
+    func avatarURL(sizedTo size: CGFloat) -> URL? {
+        if let url = avatarURL { return url }
+        avatarURL = originalAvatarURL?.resized(withSize: size)
+        return avatarURL
+    }
+}
