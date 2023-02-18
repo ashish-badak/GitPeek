f93ee614036ff1686ceaff0cabad30e848b2215c
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-18 15:08:42 +0530

    added functionality to add resizing option to content URL as used by Github

--- GitPeek/Utils/Extensions/URLExtension.swift
+++ GitPeek/Utils/Extensions/URLExtension.swift
@@ -11,4 +11,26 @@ extension URL {
     enum GP {
         static var baseURL: URL { URL(string: "https://api.github.com")! }
     }
+    
+    func resized(withSize size: CGFloat) -> URL {
+        guard var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: false)
+        else {
+            return self
+        }
+        
+        urlComponents.append(queryParams: ["s": Int(size)])
+        return urlComponents.url ?? self
+    }
+}
+
+
+extension URLComponents {
+    mutating func append(queryParams: Parameters) {
+        let newQueryItems = queryParams.map {
+            URLQueryItem(name: $0.key, value: "\($0.value)")
+        }
+        var existingQueryItems: [URLQueryItem] = self.queryItems ?? []
+        existingQueryItems.append(contentsOf: newQueryItems)
+        queryItems = existingQueryItems
+    }
 }
