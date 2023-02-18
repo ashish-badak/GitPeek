805d36f3b8297e5d805eae8f32165b8778eb0e02
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-18 17:12:24 +0530

    created PullRequestViewModel

---
+++ GitPeek/Pull Request/Presenter/View Models/PullRequestViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  PullRequestViewModel.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 18/02/23.
+//
+
+import Foundation
+
+final class PullRequestViewModel {
+    let title: String
+    let creationTime: String?
+    let closureTime: String?
+    let userViewModel: UserViewModel
+    
+    init(pullRequest: PullRequest) {
+        userViewModel = UserViewModel(user: pullRequest.user)
+        title = pullRequest.title
+        
+        if let relativeTime = pullRequest.createdAt?.relativeTime {
+            creationTime = "Created \(relativeTime)"
+        } else {
+            creationTime = nil
+        }
+        
+        if let relativeTime = pullRequest.mergedAt?.relativeTime {
+            closureTime = "Merged \(relativeTime)"
+        } else if let relativeTime = pullRequest.createdAt?.relativeTime {
+            closureTime = "Closed \(relativeTime)"
+        } else {
+            closureTime = nil
+        }
+    }
+}
