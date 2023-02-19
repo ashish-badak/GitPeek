759ec9e571a5cd4e7fa642a97e65cdc4551285c6
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-20 04:20:29 +0530

    created ImageResponseParser

--- GitPeek/Networking/Response Handlers/ImageResponseParser.swift
+++ GitPeek/Networking/Response Handlers/ImageResponseParser.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct ImageResponseParser: APIResponseParser {
+    func parse(
+        data: Data?,
+        response: URLResponse?,
+        error: Error?
+    ) -> Result<Data, Error> {
+        guard error == nil else {
+            return .failure(ImageError.fetchError)
+        }
+        
+        if let data = data {
+            return .success(data)
+        } else {
+            return .failure(ImageError.noData)
+        }
+    }
+}
