9ccc2dbff5205d6233cc06f9d72d6110d71b2a17
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-20 04:38:44 +0530

    added extension on UIImageView to set image from URL

---
+++ GitPeek/Utils/Extensions/UIImageViewExtension.swift
@@ -0,0 +1,35 @@
+//
+//  UIImageViewExtension.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 20/02/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func setImage(withURL url: URL, placeholderImageName: String = "") {
+        image = nil
+        
+        if !placeholderImageName.isEmpty {
+            let placeholderImage = UIImage(named: placeholderImageName)
+            image = placeholderImage
+        }
+        
+        ImageStore.shared.fetchImage(for: url) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let image):
+                DispatchQueue.main.async {
+                    self.image = image
+                }
+                
+            case .failure:
+                // leave the placeholder image as it is if set
+                break
+            }
+        }
+    }
+}
