bf32d0cc7e91607c79296ad513b3f95ac7bc6193
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-20 04:21:45 +0530

    created image store with caching and downloading functionality

--- GitPeek/Storage/Image Store/ImageStore.swift
+++ GitPeek/Storage/Image Store/ImageStore.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import UIKit
+
+typealias ImageCallback = (Result<UIImage, ImageError>) -> Void
+
+final class ImageStore {
+    let downloader: ImageDownloader
+    let cache: ImageCache
+    
+    static let shared = ImageStore(
+        downloader: DefaultImageDownloader.shared,
+        cache: DefaultImageCache.shared
+    )
+    
+    init(downloader: ImageDownloader, cache: ImageCache) {
+        self.downloader = downloader
+        self.cache = cache
+    }
+    
+    public func fetchImage(
+        for url: URL,
+        completion: @escaping ImageCallback
+    ) {
+        if let cachedImage = cache.image(forKey: url) {
+            completion(.success(cachedImage))
+            return
+        }
+        
+        downloader.fetchImage(from: url) { [weak self] result in
+            switch result {
+            case .success(let image):
+                self?.cache.set(image, forKey: url)
+                completion(.success(image))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
