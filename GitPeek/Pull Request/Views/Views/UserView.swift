7c965ed777c14932b6566cf64fe8fe34e4541424
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-20 22:58:31 +0530

    created user view

---
+++ GitPeek/Pull Request/Views/Views/UserView.swift
@@ -0,0 +1,65 @@
+//
+//  UserView.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 20/02/23.
+//
+
+import UIKit
+
+final class UserView: UIView {
+    private lazy var avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.constrain(height: 60, width: 60)
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .horizontal
+        view.spacing = 12
+        view.alignment = .center
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        stackView.addArrangedSubview(avatarImageView)
+        stackView.addArrangedSubview(titleLabel)
+        addSubview(stackView)
+        stackView.layoutConstraints(
+            to: self,
+            top: .view(4),
+            leading: .view(4),
+            bottom: .view(4),
+            trailing: .view(4)
+        )
+    }
+    
+    func setData(viewModel: UserViewModel) {
+        titleLabel.text = viewModel.username
+        avatarImageView.setImage(
+            withURL: viewModel.avatarURL(
+                sizedTo: avatarImageView.frame.height
+            )
+        )
+    }
+}
+
