432ccd17924e414c67002623b326f1b45646c147
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-02-21 03:22:54 +0530

    created constants

---
+++ GitPeek/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  GitPeek
+//
+//  Created by Ashish Badak on 21/02/23.
+//
+
+import Foundation
+
+enum Constants {
+    /// Please get your key from https://github.com/settings/tokens
+    static var apiKey: String { "" }
+    static var apiVersion: String { "2022-11-28"}
+    static var contentType: String { "application/vnd.github+json" }
+}
